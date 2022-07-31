8b9c8d43feb217e8388e64efaa25f36e94050758
ante-neh <amareanteneh12@gmail.com>
2022-07-31 10:03:24 +0300

    add two numbers in a linked list

--- arthemetic subarray.py
+++ arthemetic subarray.py
@@ -4,7 +4,7 @@ class Solution:
     
         for i in range(len(l)):
             arr = sorted(nums[l[i]:r[i]+1])
-            if len(arr) >= 2 and all(arr[i] - arr[i-1] == arr[1] - arr[0] for i in             range(1,len(arr))):
+            if len(arr) >= 2 and all(arr[i] - arr[i-1] == arr[1] - arr[0] for i in range(1,len(arr))):
                 res.append(True)
             else:
                 res.append(False)
