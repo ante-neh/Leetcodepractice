73cec7191c058e422d5467b2f6e9f1c5b56e8d42
ante-neh <amareanteneh12@gmail.com>
2022-08-10 04:34:05 +0300

    two pointer

---
+++ competetiveProgrammingContest/FindPairSumInRotatedAndSortedArry/soluton.py
@@ -0,0 +1,16 @@
+def findPairSum(arr, target):
+    for i in range(len(arr)-1):
+        if arr[i]>arr[i+1]:
+            break
+    l=(i+1)%len(arr)
+    r=i
+    while l!=r:
+        if arr[l]+arr[r]==target:
+            return True
+        if arr[l]+arr[r]<target:
+            l=(l+1)%len(arr)
+        else:
+            r=(len(arr)+r-1)%len(arr)
+    return False
+#time complexity O(n)
+#space complexity O(1)
