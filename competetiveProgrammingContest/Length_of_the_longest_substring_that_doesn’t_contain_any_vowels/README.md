b608a13a4b875cd24c8170cee7316cbef3654967
ante-neh <amareanteneh12@gmail.com>
2022-08-20 23:23:19 +0300

    two pointer

--- competetiveProgrammingContest/Length_of_the_longest_substring_that_doesn’t_contain_any_vowels/README.md
+++ competetiveProgrammingContest/Length_of_the_longest_substring_that_doesn’t_contain_any_vowels/README.md
@@ -1,3 +1,4 @@
+Length of the longest substring that doesn’t contain any vowels
 Input: s = "codeforintelligents"
 Output: 3
 Explanation: 'nts' is the longest substring that doesn't contain any vowels.
---
+++ competetiveProgrammingContest/Length_of_the_longest_substring_that_doesn’t_contain_any_vowels/solution.py
@@ -0,0 +1,14 @@
+def getLongestSubstring(s):
+    vowels=['a','e','i','o','u']
+    l,r=0,0
+    maxLen=0
+    while r<len(s):
+        if s[r] not in vowels:
+            r+=1
+        else:
+            r+=1
+            l=r
+        maxLen=max(maxLen,r-l)
+    return maxLen
+str = ""
+print(getLongestSubstring(str))
