acd8bdb8f26f2843665ab3bfe523e13479ae47e5
ante-neh <amareanteneh12@gmail.com>
2022-08-20 17:04:13 +0300

    sliding window

---
+++ competetiveProgrammingContest/Find_the_longest_substring_of_a_string_containing_k_distinct_characters/solution.py
@@ -0,0 +1,16 @@
+def getLongest(s, k):
+    l,r=0,0
+    sCount={}
+    maxLen=0
+    for r in range(len(s)):
+        sCount[s[r]]=1+sCount.get(s[r],0)
+        while len(sCount)>k:
+            sCount[s[l]]-=1
+            if sCount[s[l]]==0:
+                sCount.pop(s[l])
+            l+=1
+        maxLen=max(maxLen,r-l+1)
+    return maxLen
+#time complexity O(n)
+#space complexity O(1)
+        
