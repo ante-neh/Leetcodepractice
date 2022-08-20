f5d569a1e1c4b9b0191717e212ce54a57fdfd510
ante-neh <amareanteneh12@gmail.com>
2022-08-20 14:00:49 +0300

    problem statement

---
+++ competetiveProgrammingContest/Difference_between_the_maximum_and_minimum_average_of_all_k-length_continuous_subarrays/README.md
@@ -0,0 +1,7 @@
+Difference between the maximum and minimum average of all k-length continuous subarrays
+
+Input: arr[ ] = {3, 8, 9, 15}, K = 2Output: 6.5
+
+Explanation: All subarrays of length 2 are {3, 8}, {8, 9}, {9, 15} and their averages are (3+8)/2 = 5.5, (8+9)/2 = 8.5, and (9+15)/2 = 12.0 respectively.
+
+Therefore, the difference between the maximum(=12.0) and minimum(=5.5) is 12.0 -5.5 = 6.5.
--- competetiveProgrammingContest/Find_All_Anagrams_in_a_String/solution2.py
+++ competetiveProgrammingContest/Find_All_Anagrams_in_a_String/solution2.py
@@ -1,7 +1,7 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         sCount,pCount={},{}
-        counter=0
+        result=[]
         for char in p:
             pCount[ord(char)-ord('a')]=1
         l=0
@@ -9,7 +9,7 @@ class Solution:
             sCount[ord(s[r])-ord('a')]=1
             if (r-l+1)==len(p):
                 if sCount==pCount:
-                    counter+=1
+                    result.append(l)
                 sCount[ord(s[l])-ord('a')]=0
                 l+=1
-        return counter
+        return result
