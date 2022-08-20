518d73377fd7fa857007780c6a04d326e36194f2
ante-neh <amareanteneh12@gmail.com>
2022-08-20 13:37:45 +0300

    sliding window another varasion

---
+++ competetiveProgrammingContest/MaximumsumsubarrayofSizek/solution2.py
@@ -0,0 +1,13 @@
+def getMaxSum(arr, k):
+    sum=0
+    for i in range(k):
+        sum+=arr[i]
+    maxSum,l=sum,0
+    for r in range(k,len(arr)):
+        sum+=arr[r]
+        sum-=arr[l]
+        maxSum=max(maxSum,sum)
+        l+=1
+    return maxSum
+
+    
