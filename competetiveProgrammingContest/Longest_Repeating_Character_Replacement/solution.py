7de7fa06bc875f0c6d46ccd9dd7e9d4ce206ac3b
ante-neh <amareanteneh12@gmail.com>
2022-08-21 14:31:32 +0300

    sliding window

---
+++ competetiveProgrammingContest/Longest_Repeating_Character_Replacement/solution.py
@@ -0,0 +1,13 @@
+def characterReplacement(self, s, k):
+    sCount={}
+    l=0
+    maxLen=0
+    for r in range(len(s)):
+        sCount[s[r]]=1+sCount.get(s[r],0)
+        while (r-l+1)-max(sCount.values())>k:
+            sCount[s[l]]-=1
+            l+=1
+        maxLen=max(maxLen,r-l+1)
+    return maxLen
+#time complexity o(n)
+#space complexity (1)
