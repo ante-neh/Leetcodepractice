929c17881d87ef12c2d4661146123144248d5b21
ante-neh <amareanteneh12@gmail.com>
2022-09-19 20:03:52 +0300

    linked list

---
+++ competetiveProgrammingContest/Intersection_of_Two_Linked_Lists/solution.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        list1,list2=headA,headB
+        while list1!=list2:
+            list1=list1.next if list1 else headB
+            list2=list2.next if list2 else headA
+        return list1
