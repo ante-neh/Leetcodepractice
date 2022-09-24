a3461e82fef70202708f3db6f6fc6390537c113e
ante-neh <amareanteneh12@gmail.com>
2022-09-25 00:10:35 +0300

    stack

---
+++ competetiveProgrammingContest/Minimum Add to Make Parentheses Valid/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        stack=[]
+        for char in s:
+            if char=='(':
+                stack.append(char)
+            else:
+                if stack and stack[-1]=='(':
+                    stack.pop()
+                else:
+                    stack.append(char)
+        return len(stack)
