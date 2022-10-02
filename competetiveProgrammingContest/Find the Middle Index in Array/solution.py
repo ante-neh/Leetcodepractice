167f044ebb029b66a723f6078efe6c53dde0cd80
ante-neh <amareanteneh12@gmail.com>
2022-10-02 20:50:12 +0300

    prefix sum

---
+++ competetiveProgrammingContest/Find the Middle Index in Array/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        total=sum(nums)
+        leftSum=0
+        for i in range(len(nums)):
+            total-=nums[i]
+            if total==leftSum:
+                return i
+            leftSum+=nums[i]
+        return -1
+        
