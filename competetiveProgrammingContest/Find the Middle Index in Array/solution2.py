4a382a48704645efe7eef35444cd93b1c84dbfdb
ante-neh <amareanteneh12@gmail.com>
2022-10-03 22:35:12 +0300

    problem statement

--- competetiveProgrammingContest/Find the Middle Index in Array/solution2.py
+++ competetiveProgrammingContest/Find the Middle Index in Array/solution2.py
@@ -2,14 +2,14 @@ class Solution:
     def findMiddleIndex(self, nums: List[int]) -> int:
         leftSum,rightSum=[],[]
         sum=0
-        for i in range(len(nums)):
-            sum+=nums[i]
+        for num in nums:
+            sum+=num
             leftSum.append(sum)
         sum=0
         for num in reversed(nums):
             sum+=num
             rightSum.append(sum)
         for i in range(len(nums)):
-            if leftSum[i]==rightSum[len(nums)-i-1]:
+            if leftSum[i]==rightSum[len(nums)-1-i]:
                 return i
         return -1
