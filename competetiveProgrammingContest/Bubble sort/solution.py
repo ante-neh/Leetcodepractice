16914d6ef2ba30d5eb99e411f30c692940000793
ante-neh <amareanteneh12@gmail.com>
2022-10-09 10:52:10 +0300

    sorting

--- competetiveProgrammingContest/Bubble sort/solution.py
+++ competetiveProgrammingContest/Bubble sort/solution.py
@@ -1,7 +1,11 @@
 class Solution:
     def bubbleSort(self,nums):
         for i in range(len(nums)):
+            swapped=False
             for j in range(len(nums)-1-i):
                 if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
+                    swapped=True
+            if not swapped:
+                break
         return nums
