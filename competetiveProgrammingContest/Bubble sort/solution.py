e6e45052f54d3731f6eef88219530b421533a28e
ante-neh <amareanteneh12@gmail.com>
2022-10-10 15:31:44 +0300

    problem statement

--- competetiveProgrammingContest/Bubble sort/solution.py
+++ competetiveProgrammingContest/Bubble sort/solution.py
@@ -1,11 +1,10 @@
-class Solution:
-    def bubbleSort(self,nums):
-        for i in range(len(nums)):
-            swapped=False
-            for j in range(len(nums)-1-i):
-                if nums[j]>nums[j+1]:
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
-                    swapped=True
-            if not swapped:
-                break
-        return nums
+def count(nums):
+    counter=0
+    for i in range(len(nums)):
+        for j in range(len(nums)-1-i):
+            if nums[j]>nums[j+1]:
+                nums[j],nums[j+1]=nums[j+1],nums[j]
+                counter+=1
+    return counter
+num=[-2,45,0,11,15]
+print(count(num))
