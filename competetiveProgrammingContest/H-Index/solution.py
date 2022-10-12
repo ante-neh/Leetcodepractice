a08ac4f71123a52fcdc2c5262ec714daa83244df
ante-neh <amareanteneh12@gmail.com>
2022-10-12 21:21:01 +0300

    sorting

---
+++ competetiveProgrammingContest/H-Index/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        hIndex=0
+        for i in range(len(citations)):
+            if citations[i]>=(len(citations)-i):
+                return len(citations)-i
+        return 0
