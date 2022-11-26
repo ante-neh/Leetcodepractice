e4588b5f61f1f2c1266dee4674391825a046afb8
ante-neh <amareanteneh12@gmail.com>
2022-11-26 09:23:06 +0300

    binary search

---
+++ competetiveProgrammingContest/Binary Search basics/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def binarySearch(self, nums, target):
+        l, r = 0, len(nums)-1
+        
+        while l<=r:
+            mid = l + (r-l)//2
+            if nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+            else:
+                return mid
+
+        return -1
