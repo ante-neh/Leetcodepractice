a535ad7df854054280f0fb6e199bcb0d345f3d24
ante-neh <amareanteneh12@gmail.com>
2022-11-26 21:26:23 +0300

    binary search

---
+++ competetiveProgrammingContest/Find Minimum in Rotated Sorted Array/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        l, r = 0, len(nums) - 1
+        
+        if nums[r] > nums[0]:
+            return nums[0]
+        
+        while l <= r:
+            m = l + (r - l) // 2
+            if nums[m] > nums[m + 1]:
+                return nums[m + 1]
+            if nums[m - 1] > nums[m]:
+                return nums[m]
+            
+            if nums[m] > nums[0]:
+                l = m + 1
+            else:
+                r = m - 1
+                
