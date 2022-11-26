6f8a891013ce85d9c45dc6172d3e690c26518e24
ante-neh <amareanteneh12@gmail.com>
2022-11-26 10:05:10 +0300

    binary search

---
+++ competetiveProgrammingContest/Find the last position of the element/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findLastPosition(self,nums,target):
+        l, r = 0, len(nums) - 1
+        lastPosition = -1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if nums[mid] == target:
+                lastPosition = mid
+                l = mid + 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return lastPosition
